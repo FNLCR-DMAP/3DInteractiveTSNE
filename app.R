31f0c6727c4473c2231565fc6680562c4109bb06
Chen <hedy.chen.131@gmail.com>
2023-08-21 17:40:04 -0400

    test

--- app.R
+++ app.R
@@ -73,11 +73,11 @@ server <- function (input, output, session) {
   # df = df %>% filter(!is.na(pk))
   
   auth_token <- session$userData$auth0_credentials$access_token
-  rid = "ri.foundry.main.dataset.cc20947e-23ea-4e0e-a3eb-e6badeb94221"
-  url2 <- paste0("https://nidap.nih.gov/api/v1/datasets/",rid)
+  rid = "ri.foundry.main.dataset.5c075c3b-8195-48ca-aac1-a556f4f96403"
+  fileName = "spark_part-00000-e7447c17-60bc-442d-ba6d-8c2126c12be4-c000.snappy.parquet"
+  url2 <- paste0("https://nidap.nih.gov/api/v1/datasets/",rid,"/files/",fileName,"/content")
   response <- GET(url2, httr::add_headers(Authorization = paste("Bearer", auth_token)))
-  raw = content(response, as="text")
-  print(raw)
+  print(status_code(response))
   
   df = generate_random_sample_data(50000) # takes total number of points as an argument
   
