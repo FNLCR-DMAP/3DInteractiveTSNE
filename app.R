e42921ca71d9277961ba070dda845df68074161e
Thomas French <frenchth@nih.gov>
2023-09-08 08:58:04 -0400

    custom auth0 server and ui

--- app.R
+++ app.R
@@ -73,7 +73,7 @@ ui <-  fluidPage(
 )
 
 server <- function (input, output, session) {
-  observe({
+  observe({ 
     url_search_params <- parseQueryString(session$clientData$url_search)
     #print("url_search_params")
     #print(url_search_params)
@@ -406,4 +406,107 @@ server <- function (input, output, session) {
   })
 }
 
+#taken from auth0.R for scope
+has_auth_code <- function(params, state) {
+  is.null(params$error) && !is.null(params$code) && params$state == state
+}
+
+auth0_server_verify <- function(session, app, api, state) {
+  
+  u_search <- session[["clientData"]]$url_search
+  params <- shiny::parseQueryString(u_search)
+  
+  if (has_auth_code(params, state)) {
+    cred <- httr::oauth2.0_access_token(api, app(redirect_uri), params$code)
+    token <- httr::oauth2.0_token(
+      app = app(redirect_uri), endpoint = api, cache = FALSE, credentials = cred,
+      user_params = list(grant_type = "authorization_code"))
+    
+    userinfo_url <- sub("authorize", "userinfo", api$authorize)
+    resp <- httr::RETRY(
+      verb = "GET"
+      , url = userinfo_url
+      , httr::config(token = token)
+      , times = 5
+    )
+    
+    assign("auth0_credentials", token$credentials, envir = session$userData)
+    assign("auth0_info", httr::content(resp, "parsed"), envir = session$userData)
+  }
+  
+}
+#auth0_state
+#url_search_params <- parseQueryString(session$clientData$url_search)
+my_auth0_ui <- function(ui, info) {
+  print("my auth0 UI called")
+  if (missing(info)) info <- auth0_info()
+  function(req) {
+    verify <- has_auth_code(shiny::parseQueryString(req$QUERY_STRING), info$state)
+    print("auth0 ui verify")
+    print(verify)
+    
+    if (!verify) {
+      if (grepl("error=unauthorized", req$QUERY_STRING)) {
+        redirect <- sprintf("location.replace(\"%s\");", logout_url())
+        shiny::tags$script(shiny::HTML(redirect))
+      } 
+      else {
+        params <- shiny::parseQueryString(req$QUERY_STRING)
+        params$code <- NULL
+        params$state <- NULL
+        
+        query <- paste0("/?", paste(
+          mapply(paste, names(params), params, MoreArgs = list(sep = "=")),
+          collapse = "&"))
+        if (!is.null(info$remote_url) && info$remote_url != "" && !getOption("auth0_local")) {
+          redirect_uri <- info$remote_url
+        } else {
+          if (grepl("127.0.0.1", req$HTTP_HOST)) {
+            redirect_uri <- paste0("http://", gsub("127.0.0.1", "localhost", req$HTTP_HOST, query))
+          } else {
+            redirect_uri <- paste0("http://", req$HTTP_HOST, query)
+          }
+        }
+        redirect_uri <<- redirect_uri
+        
+        query_extra <- if(is.null(info$audience)) list() else list(audience=info$audience)
+        url <- httr::oauth2.0_authorize_url(
+          info$api, info$app(redirect_uri), scope = info$scope, state = info$state,
+          query_extra=query_extra
+        )
+        redirect <- sprintf("location.replace(\"%s\");", url)
+        shiny::tags$script(shiny::HTML(redirect))
+      }
+    } 
+    else {
+      if (is.function(ui)) {
+        ui(req)
+      } 
+      else {
+        ui
+      }
+    }
+  }
+}
+
+my_auth0_server <- function(server, info) {
+  print("using my auth0 server")
+  if (missing(info)) info <- auth0_info()
+  function(input, output, session) {
+    print("funciton wihin myauth0server")
+    shiny::isolate(auth0_server_verify(session, info$app, info$api, info$state))
+    shiny::observeEvent(input[["._auth0logout_"]], logout())
+    observe{
+      url_search_params <- parseQueryString(session$clientData$url_search)
+      print("search params")
+      print(url_search_params)
+    }
+    server(input, output, session)
+  }
+}
+assignInNamespace("auth0_ui", my_auth0_ui, ns = "auth0")
+assignInNamespace("auth0_server", my_auth0_server, ns = "auth0")
+
 shinyAppAuth0(ui = ui, server = server)
+
+
