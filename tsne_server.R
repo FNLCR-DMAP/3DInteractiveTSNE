eeb0685175bfac55e433e62d6b5e077795e69668
Thomas French <frenchth@nih.gov>
2023-10-18 12:32:39 -0400

    syntax

--- tsne_server.R
+++ tsne_server.R
@@ -29,9 +29,9 @@ tsne_server <- function (input, output, session, session_info = NULL) {
       )
       return(NULL)
     }
-    withProgress(message="Downloading Data From NIDAP", value = 0), {
+    withProgress(message="Downloading Data From NIDAP", value = 0, {
       df = downloa_dataset_from_nidap(rid, auth_token, branch)
-    }
+    })
     return(df)
   })
 
