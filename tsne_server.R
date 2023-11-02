6955e9ca66f66782a3e5004046a02fa6b1521cd0
Thomas French <frenchth@nih.gov>
2023-11-02 13:22:29 -0400

    input data reactiveval

--- tsne_server.R
+++ tsne_server.R
@@ -36,89 +36,96 @@ tsne_server <- function (input, output, session, session_info = NULL) {
   #     }
   # })
 
-  #inputData <- reactiveVal(NULL)
+  inputData <- reactiveVal(NULL)
+
   mydata <- reactive({
-    df <- NULL
-    print("session info")
-    print(session_info)
-    cookie <- cookies::get_cookie(session_info$state)
-    
-    print("cookie")
-    print(cookie)
-    if (!is.null(cookie)) {
-      cookie_data <- fromJSON(cookie)
+    if(!is.null(inputData())){
+      return(inputData())
+    }
+    else {
+        
+      df <- NULL
+      print("session info")
+      print(session_info)
+      cookie <- cookies::get_cookie(session_info$state)
+      
+      print("cookie")
+      print(cookie)
+      if (!is.null(cookie)) {
+        cookie_data <- fromJSON(cookie)
+        dataset_rid <- cookie_data$inputRID
+        branch <- cookie_data$inputBranch
+        output$error_message_box <- renderText(paste("Found cookie with input dataset rid : ", dataset_rid, "branch:", branch))
+        output$upload_error_message_box <- renderText(paste("Uploading to dataset:", cookie_data$outputRID))
+      } else {
+        cookie_data <- NULL
+        print(paste("could not find cooke: ", session_info$state))
+        output$error_message_box <- renderText(
+          paste("ERROR: Could not find cookie with input dataset rid. State: ", session_info$state)
+        )
+        output$upload_error_message_box <- renderText(
+          paste("ERROR: Could not find cookie with input dataset rid. State: ", session_info$state)
+        )
+        return(NULL)
+      }
+        
       dataset_rid <- cookie_data$inputRID
       branch <- cookie_data$inputBranch
-      output$error_message_box <- renderText(paste("Found cookie with input dataset rid : ", dataset_rid, "branch:", branch))
-      output$upload_error_message_box <- renderText(paste("Uploading to dataset:", cookie_data$outputRID))
-    } else {
-      cookie_data <- NULL
-      print(paste("could not find cooke: ", session_info$state))
-      output$error_message_box <- renderText(
-        paste("ERROR: Could not find cookie with input dataset rid. State: ", session_info$state)
-      )
-      output$upload_error_message_box <- renderText(
-        paste("ERROR: Could not find cookie with input dataset rid. State: ", session_info$state)
-      )
-      return(NULL)
-    }
+      withProgress(message="Downloading Data From NIDAP", value = 0, {
       
-    dataset_rid <- cookie_data$inputRID
-    branch <- cookie_data$inputBranch
-    withProgress(message="Downloading Data From NIDAP", value = 0, {
-    
-      list_files_url <- paste0("https://nidap.nih.gov/api/v1/datasets/",dataset_rid,"/files?branchId=", branch)
-      print(paste("making request to ", list_files_url))
+        list_files_url <- paste0("https://nidap.nih.gov/api/v1/datasets/",dataset_rid,"/files?branchId=", branch)
+        print(paste("making request to ", list_files_url))
 
-      response <- GET(list_files_url, httr::add_headers(Authorization = paste("Bearer", auth_token)))
-      
-      incProgress(0.10, detail="Listed files from dataset")
-      
-      data_content <- content(response, as="text")
-      parsed_json <- fromJSON(data_content)
-      files <- parsed_json$data$path
-      files <- files[!file_ext(files) %in% c("log", "")] #filter out log and spark success files
-      
-      num_files <- length(files)
-      if (num_files == 0 ){
-          stop("Error, zero files found in dataset")
-      }
-      print("reading through files")
-      df = data.frame()
-      index <- 0
-
-      for (file in files) {
-        print(paste("getting data from", file))
-        file <- url_encode(file)
-        get_file_content_url <- paste0("https://nidap.nih.gov/api/v1/datasets/",dataset_rid,"/files/",file,"/content?branchId=", branch)
-        response2 <- GET(get_file_content_url, httr::add_headers(Authorization = paste("Bearer", auth_token)))
+        response <- GET(list_files_url, httr::add_headers(Authorization = paste("Bearer", auth_token)))
         
-        if (file_ext(file) == "csv") {
-          raw <- content(response2, as="text")
-          dataset <- read.csv(text = raw)
-          dataset <- data.frame(dataset)
-          df <- rbind(df, dataset)
-        } else if (file_ext(file) == "parquet") {
-          raw = content(response2, as="raw")
-          dataset = read_parquet(raw)
-          dataset = data.frame(dataset)
-          
-          dataset$name <- file
-          df <- rbind(df, dataset)
-        } else {
-          dataset = generate_random_sample_data(100)
-          dataset$name <- "else"
-          df <- rbind(df, dataset)
+        incProgress(0.10, detail="Listed files from dataset")
+        
+        data_content <- content(response, as="text")
+        parsed_json <- fromJSON(data_content)
+        files <- parsed_json$data$path
+        files <- files[!file_ext(files) %in% c("log", "")] #filter out log and spark success files
+        
+        num_files <- length(files)
+        if (num_files == 0 ){
+            stop("Error, zero files found in dataset")
         }
-        index <- index + 1
-        incProgress(0.9 / num_files, detail=paste("Downloaded", index + 1, "of", num_files, "files"))
-      }
-      # df = df %>% filter(!is.na(pk))
-      print("successfully read in all data")
-      print(head(df, 5))
-    }) #withProgress
-    #inputData(df)
-    return(df)
+        print("reading through files")
+        df = data.frame()
+        index <- 0
+
+        for (file in files) {
+          print(paste("getting data from", file))
+          file <- url_encode(file)
+          get_file_content_url <- paste0("https://nidap.nih.gov/api/v1/datasets/",dataset_rid,"/files/",file,"/content?branchId=", branch)
+          response2 <- GET(get_file_content_url, httr::add_headers(Authorization = paste("Bearer", auth_token)))
+          
+          if (file_ext(file) == "csv") {
+            raw <- content(response2, as="text")
+            dataset <- read.csv(text = raw)
+            dataset <- data.frame(dataset)
+            df <- rbind(df, dataset)
+          } else if (file_ext(file) == "parquet") {
+            raw = content(response2, as="raw")
+            dataset = read_parquet(raw)
+            dataset = data.frame(dataset)
+            
+            dataset$name <- file
+            df <- rbind(df, dataset)
+          } else {
+            dataset = generate_random_sample_data(100)
+            dataset$name <- "else"
+            df <- rbind(df, dataset)
+          }
+          index <- index + 1
+          incProgress(0.9 / num_files, detail=paste("Downloaded", index + 1, "of", num_files, "files"))
+        }
+        # df = df %>% filter(!is.na(pk))
+        print("successfully read in all data")
+        print(head(df, 5))
+      }) #withProgress
+      inputData(df)
+      return(df)
+    }
   }) #reactive mydata
   
 
